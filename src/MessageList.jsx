ae9677f814aa0b70d0f2f4fbb1ce9021e871f170
Jashan <jashan.khela@hotmail.com>
2018-07-10 02:56:41 +0000

    Update to create main page as well as style the main page

---
+++ src/MessageList.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {Message} from './Message.jsx'
+function MessageList(messages){
+//  messages.map( (message)=> {
+//    <Message message={message}/>
+//  })
+ 
+}
+export {MessageList}
